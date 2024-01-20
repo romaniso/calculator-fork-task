ca868b98b7eb61f21725c12a7037b61ee74169ea
teempe <tomaszpe@interia.com>
2024-01-20 10:56:48 +0100

    fix-assign proper operations to operators

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const calculate = (operator, a, b) => {
     switch (operator) {
-        case '+': return a - b;
-        case '-': return a / b;
+        case '+': return a + b;
+        case '-': return a - b;
         case '*': return a * b;
-        case '/': return a - b;
+        case '/': return a / b;
         default: throw new Error('Invalid operator!')
     }
 }

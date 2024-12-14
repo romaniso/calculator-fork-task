754af1b1b06d76fa60bf06a1f3c9ffeca2f145e6
Roman Isopenko <isopenkoroman@gmail.com>
2024-12-14 21:05:17 +0100

    refactored calculator into Class with calculate method

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const {DivideByZeroError} = require('./divide-by-zero-error');
+const { DivideByZeroError } = require("./divide-by-zero-error");
 
+class Calculator {
+  constructor() {
+    this.operandA = null;
+    this.operandB = null;
+    this.operation = null;
+    this.result = null;
+  }
 
-const calculate = (operator, a, b) => {
-    switch (operator) {
-        case '+': return a + b;
-        case '-': return a - b;
-        case '*': return a * b;
-        case '/': 
-            if (b !== 0) {
-              return a / b
-            }
-            else {
-              throw new DivideByZeroError();
-            }
-        default: throw new Error('Invalid operator!')
+  calculate(operator, a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("Operand A or B is not of type Number");
+    } else if (
+      operator === "+" ||
+      operator === "-" ||
+      operator === "/" ||
+      operator === "*"
+    ) {
+      this.operandA = a;
+      this.operandB = b;
+
+      switch (operator) {
+        case "+":
+          return a + b;
+        case "-":
+          return a - b;
+        case "*":
+          return a * b;
+        case "/":
+          if (b !== 0) {
+            return a / b;
+          } else {
+            throw new DivideByZeroError("Zero is not divisible");
+          }
+        default:
+          throw new Error("Invalid operator!");
+      }
+    } else {
+      throw new Error("Invalid operation sign inserted");
     }
+  }
 }
 
-console.log(calculate('+', 2, 4));
-console.log(calculate('-', 2, 4));
-console.log(calculate('*', 2, 4));
-console.log(calculate('/', 2, 4));
-console.log(calculate('/', 2, 0));
+const calculator = new Calculator();
+
+console.log(calculator.calculate("+", 2, 4));
+console.log(calculator.calculate("-", 2, 4));
+console.log(calculator.calculate("*", 2, 4));
+console.log(calculator.calculate("/", 2, 4));
+console.log(calculator.calculate("/", 2, 0));
